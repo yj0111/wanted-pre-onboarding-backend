d6490fa0e57a96295f8d2c397cc6fc8e71096236
yj0111 <ooyouj@naver.com>
2024-08-07 20:51:23 +0900

    Init: Initial commit

---
+++ pre_onboarding/src/main/java/wanted/pre_onboarding/PreOnboardingApplication.java
@@ -0,0 +1,13 @@
+package wanted.pre_onboarding;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreOnboardingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PreOnboardingApplication.class, args);
+	}
+
+}
---
+++ pre_onboarding/src/test/java/wanted/pre_onboarding/PreOnboardingApplicationTests.java
@@ -0,0 +1,13 @@
+package wanted.pre_onboarding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreOnboardingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
