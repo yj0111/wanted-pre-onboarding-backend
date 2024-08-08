cc74fcdb29f5e1a2dc62827a6aed52481c27b8e4
yj0111 <ooyouj@naver.com>
2024-08-08 19:38:33 +0900

    Test: Add update posting test code

--- pre_onboarding/src/test/java/wanted/pre_onboarding/controller/PostingControllerTest.java
+++ pre_onboarding/src/test/java/wanted/pre_onboarding/controller/PostingControllerTest.java
@@ -9,6 +9,9 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import wanted.pre_onboarding.domain.dto.request.RegistPostRequestDto;
+import wanted.pre_onboarding.domain.dto.request.UpdatePostRequestDto;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.patch;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -36,9 +39,29 @@ public class PostingControllerTest {
                 .postingDetail("원티드에서는 백엔드 개발자..")
                 .build();
 
-        mockMvc.perform(post("/posting/regist") // Corrected path to start with "/"
+        mockMvc.perform(post("/posting/regist")
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(registPostRequestDto)))
+                .andExpect(status().isOk());
+    }
+
+    @DisplayName(value = "공고 수정 테스트")
+    @Test
+    void updatePosting() throws Exception {
+        Long postingId = 1L;
+        Long companyId = 1L;
+        UpdatePostRequestDto updatePostRequestDto = UpdatePostRequestDto.builder()
+                .postingNation("미국")
+                .postingRegion("캘리포니아")
+                .postingPosition("풀스택 개발자")
+                .postingBonus("200000원")
+                .postingSkills("Java, Python")
+                .postingDetail("최신 기술 스택을 활용한...")
+                .build();
+
+        mockMvc.perform(patch("/posting/update/{companyId}/{postingId}", companyId, postingId)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(updatePostRequestDto)))
                 .andExpect(status().isOk());
     }
 }
